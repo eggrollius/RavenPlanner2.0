deba0e8534d238fcd891d10f588532f3db939457
Ethan Huynh <71572001+eggrollius@users.noreply.github.com>
2026-04-19 19:38:15 +0000

    Initialize project, and initalize db schema.

---
+++ migrations/001_init.sql
@@ -0,0 +1,31 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_type WHERE typname = 'meeting_type'
+    ) THEN
+        CREATE TYPE meeting_type AS ENUM ('lecture', 'tutorial', 'laboratory');
+    END IF;
+END$$;
+
+CREATE TABLE IF NOT EXISTS Course(
+    Id int PRIMARY KEY,
+    FacultyCode char(4) NOT NULL,
+    CourseCode char(4) NOT NULL,
+    CourseName varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS CourseOffering(
+    Id int PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Meeting(
+    crn int PRIMARY KEY,
+    CourseOfferingId int,
+    CONSTRAINT fk_CourseOffering
+        FOREIGN KEY (CourseOfferingId)
+        REFERENCES CourseOffering(Id),
+    MeetingType meeting_type,
+    DaysOfWeekMask smallint,
+    StartTimeMinutes smallint,
+    EndTimeMinutes smallint
+);
